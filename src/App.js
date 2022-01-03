9d9437323feb4441decb1f0acf7f532b1993749a
MaueDev <mauricio.rsc.mg@gmail.com>
2022-01-03 18:57:26 -0300

    Criação de rotas

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React from 'react';
-import {Switch,Route,Link} from 'react-router-dom';
-
-/*Pages*/
-import pg1 from './pages/Pagina1';
-import pg2 from './pages/Pagina2';
-import pg3 from './pages/Pagina3';
-/*Page End*/
-
-export default function App()
-{
-  return(
-    <>
-      <header>
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 
-      </header>
-      <main>
-        <Switch>
-          <Route path="/pag2" component={pg1}/>
-          <Route path="/pag1" component={pg2}/>
-          <Route path="/pag3" component={pg3}/>
-        </Switch>
-      </main>
-    </>
-  )
+
+export default function App() {
+  return (
+    <div>
+      <h1>Bookkeeper!</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import { render } from "react-dom";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import App from "./App";
 
+/*Pages*/
 
-document.querySelector("#TitlePage").textContent = "Teste";
+import Expenses from "./pages/expenses";
+import Invoices from "./pages/invoices";
+/*Pages*/
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById("root");
+render(
+<BrowserRouter>
+  <Routes>
+    <Route path="/" element={<App/>} />
+    <Route path="expenses" element={<Expenses/>} />
+    <Route path="invoices" element={<Invoices/>} />
+    
+  </Routes>
+</BrowserRouter>, 
+rootElement);
