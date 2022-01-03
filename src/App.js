a0b0b5ab3f91dc74e934c57c773160032dc7da4f
MaueDev <mauricio.rsc.mg@gmail.com>
2022-01-03 16:32:48 -0300

    Criaçao de rotas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import React from 'react';
+import {Switch,Route,Link} from 'react-router-dom';
+
+/*Pages*/
+import pg1 from './pages/Pagina1';
+import pg2 from './pages/Pagina2';
+import pg3 from './pages/Pagina3';
+/*Page End*/
+
+export default function App()
+{
+  return(
+    <>
+      <header>
 
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+      </header>
+      <main>
+        <Switch>
+          <Route path="/pag2" component={pg1}/>
+          <Route path="/pag1" component={pg2}/>
+          <Route path="/pag3" component={pg3}/>
+        </Switch>
+      </main>
+    </>
+  )
 }
-
-export default App;
